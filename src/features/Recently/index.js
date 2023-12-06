a35192d6c772814c2037760785cddf75ec0b3659
ptrdung <ptrdung2002@gmail.com>
2023-12-06 19:48:23 +0700

    Recently + About + Category + Avoid Conflic

--- src/features/Recently/index.js
+++ src/features/Recently/index.js
@@ -60,6 +60,31 @@ const Recently = () => {
             <ExpenseList key={index} {...expense} />
           ))}
         </div>
+        <button className="add-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+          >
+            <path d="M0 0H50V50H0V0Z" fill="white" fill-opacity="0.01" />
+            <path
+              d="M25.0634 10.4167L25.0251 39.5834"
+              stroke="black"
+              stroke-width="4"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M10.4167 25.0001H39.5834"
+              stroke="black"
+              stroke-width="4"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
       </div>
       {/* <div className='main-content'>
         <div className='title'>List of expenses</div>
