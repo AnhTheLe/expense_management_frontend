c8f03ed98585b7dd16df2106a0f99ee0ea5e7d4a
Lê Thế Anh-Intern <theanh19082002@gmail.com>
2023-12-13 21:49:42 +0700

    update

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,12 +3,16 @@ import axiosClient from "./axiosClient";
 const authApi = {
     signIn: (params) => {
         const url = '/users/signin';
-        return axiosClient.post(url, params);        
+        return axiosClient.post(url, params);
     },
     signUp: (params) => {
         const url = '/users/signup';
-        return axiosClient.post(url, params);        
+        return axiosClient.post(url, params);
     },
+    getCurrentUser: (params) => {
+        const url = '/user/current-user';
+        return axiosClient.get(url, {params});
+    }
     // signOut: () => {
     //     const url = '/users/sign-out';
     //     return axiosClient.post(url);
