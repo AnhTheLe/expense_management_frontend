d445cd4ff6bf9a67dfd71972aa89f057313cc363
trungduc1294 <trungduc.1294@gmail.com>
2023-12-16 00:41:54 +0700

    temp commit

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,7 +4,12 @@ const userApi = {
   getCurrentUser: (params) => {
     const url = '/user/current-user?username=' + params;
     return axiosClient.get(url);
-  }
+  },
+
+  editAccount: (id, params) => {
+    const url = '/update-user/' + id;
+    return axiosClient.put(url, params);
+  },
 };
 
 export default userApi;
