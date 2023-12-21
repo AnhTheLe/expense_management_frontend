494bba5ad2aee12a4046a5d8387ba19cb63ef9a8
LeZuy <lanhzuy2002@gmail.com>
2023-12-21 15:43:33 +0700

    statistic api

--- src/api/statisticApi.js
+++ src/api/statisticApi.js
@@ -5,6 +5,10 @@ const statisticApi = {
         const url = '/user-expenses/statistical-by-time?start_date=' + startDate + "&end_date=" +endDate;
         return axiosClient.get(url);
     },
+    getStatisticalByCategory: (startDate, endDate) => {
+        const url = '/user-expenses/statistical?start_date=' + startDate + "&end_date=" +endDate;
+        return axiosClient.get(url);
+    },
 }
 
 export default statisticApi;
